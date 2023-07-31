0becfff333f47d6f99500f070234a08a3607d546
regismael <ismael.l.regis@gmail.com>
2023-07-31 12:40:00 -0300

    feat: desenvolvimento da consulta da produtos

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    /* Mapeamento do endpoint da API de produtos */
+    apiProdutos: 'http://produtosapicoti-001-site1.ctempurl.com/api/produtos',
+  };
+  
+  
+  
+  
+  
